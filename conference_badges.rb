d210dd4e3c0971c6aaffc58eaa9c6f47e32dac73
Richard Kim <richeekim@gmail.com>
2018-03-29 20:47:39 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,23 @@
-# Write your code here.
+def
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  for x in 0... attendees.length
+    attendees[x] = "Hello, my name is #{attendees[x]}."
+  end
+  return attendees
+end
+
+def assign_rooms(attendees)
+  for x in 0...attendees.length
+    attendees[x] = "Hello, #{attendees[x]}! You'll be assigned to room #{x+1}!"
+  end
+  return attendees
+end
+
+def printer(attendees)
+  for i in 0...attendees.length
+  puts batch_badge_creator(attendees[i])
+end
+end
